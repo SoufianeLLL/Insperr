e94fda210fde50f06f1c64666ca08569e31d7eb0
Soufiane L <soufiane.loudini@gmail.com>
2022-10-24 20:38:12 +0100

    fix Stripe hooks request to supabase

--- pages/api/stripe/hooks.tsx
+++ pages/api/stripe/hooks.tsx
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
 
 			if (id) {
 				await supabaseAdmin
-					.from('subscription')
+					.from('logs')
 					.insert({
 						log: event?.data?.object
 					})
