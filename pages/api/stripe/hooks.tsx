cf77746ac6cf4cbc7c7357b1e173b725e75457b7
Soufiane L <soufiane.loudini@gmail.com>
2022-10-24 20:06:44 +0100

    fix Stripe hooks request to supabase

--- pages/api/stripe/hooks.tsx
+++ pages/api/stripe/hooks.tsx
@@ -7,65 +7,78 @@ export const config = { api: { bodyParser: false } }
 
 export default async function handler(req, res) {
 
-    const signature = req.headers["stripe-signature"]
-    const signingSecret = process.env.STRIPE_SIGNING_SECRET_TEST
-    const reqBuffer = await buffer(req)
+	const signature = req.headers["stripe-signature"]
+	const signingSecret = process.env.STRIPE_SIGNING_SECRET_TEST
+	const reqBuffer = await buffer(req)
 
-    let event
+	let event
 
-    try {
-        event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret)
-    }
-    catch (error) {
-        return res.status(400).send(`Webhook error: ${error.message}`)
-    }
+	try {
+		event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret)
+	}
+	catch (error) {
+		return res.status(400).send(`Webhook error: ${error.message}`)
+	}
 
-    
-    switch (event.type) {
-        case "customer.subscription.created":
-            // Get user ID by customer ID
-            const { data: { id } } = await supabaseAdmin
-                .from('users')
-                .select('id')
-                .eq('stripe_customer_id', event.data.object.customer)
-                .single()
+	
+	switch (event?.type) {
+		// Update subscription status when it's active
+		case "customer.subscription.updated":
+			if (event?.data?.object?.status === 'active') {
+				await supabaseAdmin
+					.from('subscription')
+					.update({
+						is_subscribed: true
+					})
+					.eq('customer_id', event?.data?.object?.customer)
+			}
+			break;
+		
+		// Create new subscription
+		case "customer.subscription.created":
+			// Get user ID by customer ID
+			const { data: { id } } = await supabaseAdmin
+				.from('users')
+				.select('id')
+				.eq('stripe_customer_id', event?.data?.object?.customer)
+				.single()
 
+			if (id) {
+				await supabaseAdmin
+					.from('subscription')
+					.insert({
+						user_id: id,
+						customer_id: event?.data?.object?.customer,
+						is_subscribed: event?.data?.object?.status === 'active' ? true : false,
+						metadata: {
+							subscription_id: event?.data?.object?.id,
+							status: event?.data?.object?.status,
+							invoice_id: event?.data?.object?.latest_invoice,
+							amount: event?.data?.object?.items?.data[0]?.plan?.amount,
+							currency: event?.data?.object?.currency,
+							interval: event?.data?.object?.items.data[0].plan.interval,
+							interval_count: event?.data?.object?.items.data[0].plan.interval_count,
+							current_period_end: event?.data?.object?.current_period_end,
+							current_period_start: event?.data?.object?.current_period_start,
+						},
+						price_id: event?.data?.object?.items.data[0].plan.id,
+						product_id: event?.data?.object?.items.data[0].plan.product
+					})
+			} 
+			else {
+				return res.status(400).send(`An error was occured, please contact support.`)
+			}
+			break;
+		
+		// Delete subscription
+		case "customer.subscription.deleted":
 			await supabaseAdmin
-				.from('logs')
-				.insert({ log: event })
+				.from('subscription')
+				.delete()
+				.eq('customer_id', event?.data?.object?.customer)
+				.eq('metadata->subscription_id', event?.data?.object?.id)
+			break;
+	}
 
-			await supabaseAdmin
-				.from('logs')
-				.insert({ log: id })
-
-            // if (id) {
-            //     await supabaseAdmin
-            //         .from('subscription')
-            //         .insert({
-            //             user_id: id,
-            //             customer_id: event.data.object.customer,
-            //             is_subscribed: event.data.object.paid,
-            //             metadata: {
-            //                 payment_status: event?.data?.object?.payment_status,
-            //                 amount_subtotal: event?.data?.object?.amount_subtotal,
-            //                 amount_total: event?.data?.object?.amount_total,
-            //                 currency: event?.data?.object?.currency,
-            //                 email: event?.data?.object?.customer_details?.email,
-            //                 name: event?.data?.object?.customer_details?.name,
-            //                 next_payment: event?.data?.object?.next_payment_attempt,
-            //                 interval: event.data.object.items.data[0].plan.interval,
-            //                 interval_count: event.data.object.items.data[0].plan.interval_count,
-            //                 // interval: event.data.object.items.data[0].plan.interval,
-            //             },
-            //             price_id: event.data.object.items.data[0].plan.id,
-            //             product_id: event.data.object.items.data[0].plan.product
-            //         })
-            // } 
-            // else {
-            //     return res.status(400).send(`An error was occured, please contact support.`)
-            // }
-          break;
-    }
-
-    res.send({ received: true })
+	res.send({ received: true })
 }
